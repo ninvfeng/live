dccc3550c7b1a1879056232346876a0ae9d08605
ninvfeng <ninvfeng@qq.com>
2018-02-07 16:01:43 +0800

    验证器优化

--- app/controller/index.php
+++ app/controller/index.php
@@ -3,6 +3,9 @@ namespace app\controller;
 class index{
 
     public function index(){
-        echo "Hello live php";
+        $id=get('id','require|min:5','请填写ID且最小长度为5');
+        $name=get('name','require','名称必填');
+        dump($id);
+        dump($name);
     }
 }
--- function.php
+++ function.php
@@ -35,6 +35,98 @@ function db($table='null'){
     }
 }
 
+//实例化验证器
+function validate($rules=[],$message=[]){
+    static $_validate;
+    $_validate=new think\Validate();
+    $_validate->rule($rules)->message($message);
+    return $_validate;
+}
+
+//响应请求
+function response($data,$msg="数据为空",$code='400'){
+    if(isset($data)&&(!empty($data))){
+        $code=200;
+        $msg='success';
+    }
+    header('Content-type: application/json');
+    $arr = array(
+        'data'=>$data,
+        'code'=>$code,
+        'message'=>$msg,
+    );
+    echo json_encode($arr);
+    exit();
+}
+
+//获取get参数
+function get($key='null',$rule='null',$message=[]){
+    if($key==='null'){
+        if($rule==='null'){
+            return $_GET;
+        }else{
+            if(!validate($rule,$message)->check($_GET)){
+                response(null,validate()->getError());
+            }
+            return $_GET;
+        }
+    }else{
+        $key2=explode('.',$key);
+        $res=$_GET;
+        foreach($key2 as $k => $v){
+            $res=$res[$v];
+        }
+        if($rule==='null'){
+            return $res;
+        }else{
+            if(!validate([$key=>$rule])->check([$key=>$res])){
+                if($message){
+                    response(null,$message);
+                }
+                response(null,validate()->getError());
+            }
+            return $res;
+        }
+    }
+}
+
+//获取post参数
+function post($key='null',$rule='null',$message=[]){
+    $data = $_POST;
+    foreach ($data as $k =>$v){
+        if(substr($v,0,1)==':'){
+            $data[$k] = substr_replace($v,'',0,1);
+        }
+    }
+    if($key==='null'){
+        if($rule==='null'){
+            return $data;
+        }else{
+            if(!validate($rule,$message)->check($data)){
+                response(null,validate()->getError());
+            }
+            return $data;
+        }
+    }else{
+        $key2=explode('.',$key);
+        $res=$data;
+        foreach($key2 as $k => $v){
+            $res=$res[$v];
+        }
+        if($rule==='null'){
+            return $res;
+        }else{
+            if(!validate([$key=>$rule])->check([$key=>$res])){
+                if($message){
+                    response(null,$message);
+                }
+                response(null,validate()->getError());
+            }
+            return $res;
+        }
+    }
+}
+
 //http请求
 function http($url, $params = array(), $method = 'GET', $ssl = false){
     $opts = array(
